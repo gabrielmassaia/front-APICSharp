eb76542f53de73d002803ae75993f4a7c1fd7eeb
gabriel.massaia <gabriel.massaia@movtech.com>
2025-02-05 21:33:55 -0300

    pessoa.ts criada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pessoa } from './models/pessoa';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Consumir-Api';
+  http = inject(HttpClient);
+  urlApi = 'https://localhost:7251';
+  pessoas: Pessoa[] = [];
+
+  ngOnInit(): void {
+    this.obterPessoas();
+  }
+
+  obterPessoas() {
+    this.http.get<Pessoa[]>(`${this.urlApi}/pessoas`)
+      .subscribe(pessoas => this.pessoas = pessoas)
+  }
 }
